324d85b4f2f7a2806290fbe00f6d9ec8b2e56e5c
zhangyijing <344796884@qq.com>
2022-04-21 11:03:05 +0800

    init

---
+++ src/main/java/com/randomproject/RandomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.randomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/randomproject/RandomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
