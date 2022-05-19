59559a47bdcd471df001cfbb2b64b0f240d135e4
zhangyijing0302 <344796884@qq.com>
2022-05-19 23:07:41 +0800

    阻塞队列入门

---
+++ src/main/java/com/randomProject/threadAndLock/BlockingQuueCase.java
@@ -0,0 +1,18 @@
+package com.randomProject.threadAndLock;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+public class BlockingQuueCase {
+
+    public static void main(String[] args) {
+        BlockingQueue array = new ArrayBlockingQueue(1);
+        BlockingQueue licked = new LinkedBlockingQueue();
+        BlockingQueue sync = new SynchronousQueue();
+
+
+    }
+
+}
