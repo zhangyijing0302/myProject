4025125030299d4ad487daf1e2165351a7515797
zhangyijing <zhangyijing@croot.com>
2022-05-20 11:34:36 +0800

    DEMO

--- sql/mysql/sql语句.sql
+++ sql/mysql/sql语句.sql
@@ -1,3 +1,9 @@
+show full PROCESSLIST
+
+-- innodb事务表 查看当前正在运行中的事务
+select * from information_schema.innodb_trx
+kill #{mysql_thread_id}
+
 -- 主键冲突三种解决方案
 insert into student (id, stuname, age) values (1,"张三", 12) on duplicate key update stuname = '张三', age = 12;
 
--- src/main/java/com/randomProject/temp/Test01.java
+++ src/main/java/com/randomProject/temp/Test01.java
@@ -3,6 +3,9 @@ package com.randomProject.temp;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Optional;
+
 
 /**
  * @author ZhangYijing
@@ -15,11 +18,21 @@ public class Test01 {
 
     @Test
     public void test() {
-        System.out.println("aaa");
+        String str = "H";
+        boolean b = false;
+        Boolean aBoolean = Optional.ofNullable(str).map("H"::equals).orElse(false);
+        System.out.println(aBoolean);
+    }
+
+    @Test
+    public void test2() {
+        int i = -100;
+        System.out.println(~(i - 1));
     }
 
     public static void main(String[] args) {
-
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        hashMap.put(1,2);
     }
 
 }
