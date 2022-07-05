0769e551b94cd9064ea1c09975fad562e93ba99b
zhangyijing0302 <344796884@qq.com>
2022-07-05 23:33:44 +0800

    forkjoin

---
+++ src/main/java/com/randomProject/java/forkjoin/ForkJoinDemo.java
@@ -0,0 +1,9 @@
+package com.randomProject.java.forkjoin;
+
+public class ForkJoinDemo {
+    public static void main(String[] args) {
+//        for (int i = 0; i <= 10_000 0_0000; i++) {
+//
+//        }
+    }
+}
